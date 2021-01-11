ccac28dc548cb58ce402287da1e8f67bdb591557
sadeghpro <sadeghmohamadnia@yahoo.com>
2021-01-11 19:31:29 +0330

    Add Persian language and remove unused languages

--- app/i18n/index.js
+++ app/i18n/index.js
@@ -6,57 +6,14 @@ export * from './isRTL';
 
 export const LANGUAGES = [
 	{
+		label: 'فارسی',
+		value: 'fa',
+		file: require('./locales/fa').default
+	},
+	{
 		label: 'English',
 		value: 'en',
 		file: require('./locales/en').default
-	}, {
-		label: '简体中文',
-		value: 'zh-CN',
-		file: require('./locales/zh-CN').default
-	}, {
-		label: '繁體中文',
-		value: 'zh-TW',
-		file: require('./locales/zh-TW').default
-	}, {
-		label: 'Deutsch',
-		value: 'de',
-		file: require('./locales/de').default
-	}, {
-		label: 'Español (ES)',
-		value: 'es-ES',
-		file: require('./locales/es-ES').default
-	}, {
-		label: 'Français',
-		value: 'fr',
-		file: require('./locales/fr').default
-	}, {
-		label: 'Português (BR)',
-		value: 'pt-BR',
-		file: require('./locales/pt-BR').default
-	}, {
-		label: 'Português (PT)',
-		value: 'pt-PT',
-		file: require('./locales/pt-PT').default
-	}, {
-		label: 'Russian',
-		value: 'ru',
-		file: require('./locales/ru').default
-	}, {
-		label: 'Nederlands',
-		value: 'nl',
-		file: require('./locales/nl').default
-	}, {
-		label: 'Italiano',
-		value: 'it',
-		file: require('./locales/it').default
-	}, {
-		label: '日本語',
-		value: 'ja',
-		file: require('./locales/ja').default
-	}, {
-		label: 'العربية',
-		value: 'ar',
-		file: require('./locales/ar').default
 	}
 ];
 
@@ -68,7 +25,7 @@ const translations = LANGUAGES.reduce((ret, item) => {
 i18n.translations = translations;
 i18n.fallbacks = true;
 
-const defaultLanguage = { languageTag: 'en', isRTL: false };
+const defaultLanguage = { languageTag: 'fa', isRTL: true };
 const availableLanguages = Object.keys(i18n.translations);
 const { languageTag, isRTL } = RNLocalize.findBestAvailableLanguage(availableLanguages) || defaultLanguage;
 
